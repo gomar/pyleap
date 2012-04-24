e852b13907dd5fbbe7437ccaa054b113628b3c65
Adrien Gomar (mac) <gomar@cerfacs.fr>
2012-04-24 15:12:03 +0200

    pep8 and pylint

--- examples/bench_algo/bench_algo.py
+++ examples/bench_algo/bench_algo.py
@@ -1,60 +1,92 @@
+#
+#  bench_algo.py
+#  PyLeap
+#
+#  Created by Adrien Gomar on 2012-04-24.
+#  Copyright 2012 CERFACS. All rights reserved.
+#
+
+"""
+Script to estimate the performance of sampling
+algorithms for almost-periodic computations.
+"""
+
 from pyleap import HbComputation, HbAlgoAPFT, HbAlgoOPT, HbAlgoEQUI
 import numpy as np
 
 
-def main(f, f1, algo_choice, sampling=None):
+def conditionning_algo(f, f1, algo_choice):
+    """
+    Function that computes the best condition number
+    found by the given algorithm.
+    """
+    frequencies = [f, f1]
+    frequencies.sort()
+
     # initializing the HbComputation object with the given frequencies
-    FREQ = [f1, f]
-    FREQ.sort()
-    hb_computation = HbComputation(frequencies=FREQ)
-    
+    hb_computation = HbComputation(frequencies=frequencies)
+
     # initializing the chosen algorithm
     if algo_choice == 'OPT':
-        ALGO = HbAlgoOPT(frequencies=FREQ)
+        hb_algo = HbAlgoOPT(frequencies=frequencies)
     if algo_choice == 'APFT':
-        ALGO = HbAlgoAPFT(frequencies=FREQ)
+        hb_algo = HbAlgoAPFT(frequencies=frequencies)
     elif algo_choice == 'EQUI_2N_1':
-        ALGO = HbAlgoEQUI(frequencies=FREQ, sampling=2 * len(FREQ) + 1)
+        hb_algo = HbAlgoEQUI(frequencies=frequencies,
+                          sampling=2 * len(frequencies) + 1)
     elif algo_choice == 'EQUI_3N_1':
-        ALGO = HbAlgoEQUI(frequencies=FREQ, sampling=3 * len(FREQ) + 1)
+        hb_algo = HbAlgoEQUI(frequencies=frequencies,
+                          sampling=3 * len(frequencies) + 1)
     elif algo_choice == 'EQUI_20N_1':
-        ALGO = HbAlgoEQUI(frequencies=FREQ, sampling=20 * len(FREQ) + 1)
-    
-    # setting the optimized time levels to compute the DFT condition number
-    hb_computation.set_timelevels(ALGO.optimize_timelevels())
+        hb_algo = HbAlgoEQUI(frequencies=frequencies,
+                          sampling=20 * len(frequencies) + 1)
+
+    # setting the optimized time levels to compute the
+    # almost periodic DFT matrix condition number
+    hb_computation.set_timelevels(hb_algo.optimize_timelevels())
     return hb_computation.conditionning()
 
-if __name__ == '__main__':
-    
-    
-    def launch(ALGO_CHOICE):
-        # base frequency to compute the delta_f_star
-        f1 = 3.
-        # number of samples for delta_f_star discretization
-        SAMPLING = 1000
-        # computing delta_f_star and the corresponding f vector
-        delta_f_star = np.linspace(0.001, 1.999, num=SAMPLING)
-        f2 = f1 * (2 - delta_f_star) / (delta_f_star + 2)
 
-        # intializaing the results
-        RESU = np.empty((1000, 2))        
-        RESU[:, 0] = delta_f_star
-        
-        # vectorializing the function to run faster (C loops instead of python ones)
-        vfunc = np.vectorize(main)
-        RESU[:, 1] =  vfunc(f2, f1, ALGO_CHOICE)
-        
-        # printing global infos that are not saved to a file
-        print 'ALGO: %s' % ALGO_CHOICE
-        print 'mean', np.mean(RESU[:, 1]), 'min', np.min(RESU[:, 1]), 'max', np.max(RESU[:, 1]) 
-        print ''
-        
-        # saving the result (delta_f_star, conditionning) to a file
-        np.savetxt(fname='BENCH_ALGO_%s.dat' % ALGO_CHOICE, X=RESU)
-        
-    # launching all the algorithms   
-    launch('EQUI_2N_1')
-    launch('EQUI_3N_1')
-    launch('EQUI_20N_1')
-    launch('OPT')
-    launch('APFT')
+def main(algo_choice, sampling, f1):
+    """
+    Main function: computes the delta_f_star, and run all
+    the call to the conditionning function
+    """
+    # computing delta_f_star and the corresponding f vector
+    delta_f_star = np.linspace(0.001, 1.999, num=sampling)
+    f = f1 * (2 - delta_f_star) / (delta_f_star + 2)
+
+    # intializaing the results
+    result = np.empty((sampling, 2))
+    result[:, 0] = delta_f_star
+
+    # vectorializing the function to run faster
+    # (C loops instead of python ones)
+    vfunc = np.vectorize(conditionning_algo)
+    result[:, 1] = vfunc(f, f1, algo_choice)
+
+    # printing global infos that are not saved to a file
+    print 'ALGO: %s' % algo_choice
+    print ('mean', np.mean(result[:, 1]),
+           'min', np.min(result[:, 1]),
+           'max', np.max(result[:, 1]))
+    print ''
+
+    # saving the result (delta_f_star, conditionning) to a file
+    np.savetxt(fname='BENCH_ALGO_%s.dat' % algo_choice, X=result)
+
+if __name__ == '__main__':
+
+    # base frequency to compute the delta_f_star
+    # delta_f_star is defined as
+    # delta_f_star = 2*(f1-f)/(f1+f)
+    F1 = 3.
+    # number of samples for delta_f_star discretization
+    SAMPLING = 1000
+
+    # launching all the algorithms
+    for algo in ['EQUI_2N_1', 'EQUI_3N_1',
+                        'EQUI_20N_1', 'OPT', 'APFT']:
+        main(algo_choice=algo,
+             sampling=SAMPLING,
+             f1=F1)
